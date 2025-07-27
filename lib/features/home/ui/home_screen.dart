fb15eb72fc055f9b823edf056af974d961d77883
hussein shafeek <99825523+hussein-shafeek@users.noreply.github.com>
2025-07-27 15:45:01 +0300

    implement profile tap ui

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:evently/core/theme/app_colors.dart';
 import 'package:evently/features/events/ui/love_tab.dart';
 import 'package:evently/features/events/ui/map_tap.dart';
-import 'package:evently/features/events/ui/profile_tab.dart';
+import 'package:evently/features/events/ui/profile/profile_tab.dart';
 import 'package:evently/features/home/data/nav_bar_icon.dart';
 import 'package:evently/features/home/ui/home_tab/home_tab.dart';
 import 'package:flutter/material.dart';
